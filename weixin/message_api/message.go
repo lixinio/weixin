956d46b0a912306de93ae3d00917ba9aa8ceffd1
king <qiujinwu@gmail.com>
2023-02-03 18:10:45 +0800

    微信模板消息

--- weixin/message_api/message.go
+++ weixin/message_api/message.go
@@ -7,7 +7,8 @@ import (
 )
 
 const (
-	apiCustomSend = "/cgi-bin/message/custom/send"
+	apiCustomSend   = "/cgi-bin/message/custom/send"
+	apiTemplateSend = "/cgi-bin/message/template/send"
 )
 
 type MessageApi struct{ *utils.Client }
@@ -46,4 +47,42 @@ func (api *MessageApi) SendCustomTextMessage(
 			Content: content,
 		},
 	}, nil)
+}
+
+type TemplateMessageMp struct {
+	AppID    string `json:"appid"`              // 所需跳转到的小程序appid（该小程序 appid 必须与发模板消息的公众号是绑定关联关系）
+	PagePath string `json:"pagepath,omitempty"` // 所需跳转到小程序的具体页面路径，支持带参数,（示例index?foo=bar），要求该小程序已发布
+}
+
+type TemplateMessageData struct {
+	Value string `json:"value"`
+	Color string `json:"color,omitempty"` // 模板内容字体颜色，不填默认为黑色
+}
+
+type TemplateMessage struct {
+	ToUser      string                          `json:"touser,omitempty"`        // 接收者openid
+	TemplateID  string                          `json:"template_id"`             // 模板ID
+	URL         string                          `json:"url,omitempty"`           // 模板跳转链接（海外帐号没有跳转能力）
+	MiniProgram *TemplateMessageMp              `json:"miniprogram,omitempty"`   // 跳小程序所需数据，不需跳小程序可不用传该数据
+	ClientMsgID string                          `json:"client_msg_id,omitempty"` // 防重入id。对于同一个openid + client_msg_id, 只发送一条消息,10分钟有效,超过10分钟不保证效果。若无防重入需求，可不填
+	Datas       map[string]*TemplateMessageData `json:"data"`                    // 模板数据
+}
+
+type TemplateMessageResponse struct {
+	utils.WeixinError
+	MsgID int64 `json:"msgid"`
+}
+
+/*
+发送模板消息
+https://developers.weixin.qq.com/doc/offiaccount/Message_Management/Template_Message_Interface.html
+*/
+func (api *MessageApi) SendTemplateMessage(
+	ctx context.Context, msg *TemplateMessage,
+) (int64, error) {
+	resp := &TemplateMessageResponse{}
+	if err := api.Client.HTTPPostJson(ctx, apiTemplateSend, msg, resp); err != nil {
+		return 0, err
+	}
+	return resp.MsgID, nil
 }
--- weixin/message_api/message_test.go
+++ weixin/message_api/message_test.go
@@ -2,6 +2,7 @@ package message_api
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/lixinio/weixin/test"
@@ -9,6 +10,7 @@ import (
 	"github.com/lixinio/weixin/utils/redis"
 	"github.com/lixinio/weixin/weixin/authorizer"
 	"github.com/lixinio/weixin/weixin/official_account"
+	"github.com/stretchr/testify/require"
 )
 
 type messageItem struct {
@@ -49,5 +51,50 @@ func TestCustomerMessage(t *testing.T) {
 	} {
 		messageApi := NewApi(client.Client)
 		messageApi.SendCustomTextMessage(ctx, client.OpenID, "发多了开发")
+	}
+}
+
+/*
+测试号模板
+{{title.DATA}}
+{{msg1.DATA}}
+{{msg2.DATA}}
+{{msg3.DATA}}
+{{msg4.DATA}}
+{{msg5.DATA}}
+*/
+func TestTemplateMessage(t *testing.T) {
+	ctx := context.Background()
+	for _, client := range []*messageItem{
+		initOfficialAccount(),
+		initAuthorizer(),
+	} {
+		messageApi := NewApi(client.Client)
+		id, err := messageApi.SendTemplateMessage(ctx, &TemplateMessage{
+			ToUser:     client.OpenID,
+			TemplateID: "pBP6oKHSz4UOkadAYsLw-ug5G495JRZsEVu626eJEmo",
+			Datas: map[string]*TemplateMessageData{
+				"title": {
+					Value: "标题",
+				},
+				"msg1": {
+					Value: "标题msg1",
+				},
+				"msg2": {
+					Value: "标题msg2",
+				},
+				"msg3": {
+					Value: "标题msg3",
+				},
+				"msg4": {
+					Value: "标题msg4",
+				},
+				"msg5": {
+					Value: "标题msg5",
+				},
+			},
+		})
+		require.Equal(t, nil, err)
+		fmt.Println(id)
 	}
 }
