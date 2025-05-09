a4473ea1942d1d60beace216297539c81188eadb
lwb <lwb2hml@gmail.com>
2025-05-09 16:43:04 +0800

    增加企微客户群相关接口；
    增加获取成员的激活详情接口；

--- wxwork_provider/license_api/account.go
+++ wxwork_provider/license_api/account.go
@@ -2,12 +2,16 @@ package license_api
 
 import (
 	"context"
+
 	"github.com/lixinio/weixin/utils"
 )
 
 const (
 	// 获取企业的账号列表
 	apiListActivedAccount = "/cgi-bin/license/list_actived_account"
+	// 获取成员的激活详情
+	// get_active_info_by_user
+	apiGetActiveInfoByUser = "/cgi-bin/license/get_active_info_by_user"
 )
 
 // ListAcitvedAccountRequest 获取企业的账号列表请求
@@ -39,6 +43,42 @@ func (api *LicenseApi) ListActivedAccount(
 	if err := api.Client.HTTPPostJson(
 		ctx,
 		apiListActivedAccount,
+		req,
+		result,
+	); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+type GetActiveInfoByUserRequest struct {
+	CorpID string `json:"corpid"`
+	UserID string `json:"userid"`
+}
+
+type GetActiveInfoByUserResponse struct {
+	utils.WeixinError
+	ActiveStatus   int `json:"active_status"`
+	ActiveInfoList []*struct {
+		ActiveCode string `json:"active_code"`
+		Type       int    `json:"type"`
+		UserID     string `json:"userid"`
+		ActiveTime int64  `json:"active_time"`
+		ExpireTime int64  `json:"expire_time"`
+	} `json:"active_info_list"`
+}
+
+// GetActiveInfoByUser 获取成员的激活详情
+func (api *LicenseApi) GetActiveInfoByUser(
+	ctx context.Context,
+	req *GetActiveInfoByUserRequest,
+) (*GetActiveInfoByUserResponse, error) {
+	result := &GetActiveInfoByUserResponse{}
+
+	if err := api.Client.HTTPPostJson(
+		ctx,
+		apiGetActiveInfoByUser,
 		req,
 		result,
 	); err != nil {
